65c9cac9dc569dc5bbab4c7415aeaf5b5a83b814
JackBecknell <52moosefit@gmail.com>
2022-03-17 12:47:58 -0400

    Created serializers for both Supers and SuperTypes

--- super_types/models.py
+++ super_types/models.py
@@ -1,4 +1,4 @@
 from django.db import models
 
-class SuperTypes(models.model):
+class SuperTypes(models.Model):
     name = models.CharField(max_length=255)
--- supers/models.py
+++ supers/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from super_types.models import SuperTypes
 
-class Supers(models.model):
-    name = models.CharField(maxlength=255)
-    alter_ego = models.CharField(maxlength=255)
-    primary_ability = models.CharField(maxlength=255)
-    secondary_ability = models.CharField(maxlength=255)
-    catchphrase = models.CharField(maxlength=255)
+class Supers(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(SuperTypes, on_delete=models.PROTECT)
